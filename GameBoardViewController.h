6cdb18ae0e7bea9ee2a0b33815c6d47751177b5b
Elliot Babchick <ebabchick@gmail.com>
2010-04-23 20:38:02 -0700

    'started'

---
+++ GameBoardViewController.h
@@ -0,0 +1,48 @@
+//
+//  GameBoardViewController.h
+//  RAMI
+//
+//  Created by Elliot Babchick on 4/23/10.
+//  Copyright 2010 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GameBoardViewController : UIViewController {
+	IBOutlet UIImageView* background_skin;
+	IBOutlet UIImageView* binary_read_1;
+	IBOutlet UIImageView* binary_read_2;
+	IBOutlet UIImageView* binary_read_3;
+	IBOutlet UIImageView* binary_read_4;
+	IBOutlet UIImageView* reset_lever;
+	IBOutlet UIImageView* tubes_1;
+	IBOutlet UIImageView* tubes_2;
+	IBOutlet UIImageView* tubes_3;
+	IBOutlet UIImageView* tubes_4;
+	IBOutlet UIImageView* buckets;
+	IBOutlet UIImageView* ball_1;
+	IBOutlet UIImageView* ball_2;
+	IBOutlet UIImageView* ball_3;
+	IBOutlet UIImageView* ball_4;
+	IBOutlet UIImageView* ball_5;
+	IBOutlet UIImageView* ball_6;
+	IBOutlet UIImageView* ball_7;
+	IBOutlet UIImageView* ball_8;
+	IBOutlet UIImageView* ball_9;
+	IBOutlet UIImageView* ball_10;
+	IBOutlet UIImageView* ball_11;
+	IBOutlet UIImageView* ball_12;
+	IBOutlet UIImageView* ball_13;
+	IBOutlet UIImageView* ball_14;
+	IBOutlet UIImageView* ball_15;
+	IBOutlet UIImageView* ball_16;
+
+
+
+
+}
+
+@property
+
+@end
